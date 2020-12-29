63ab1e7eebb32f01c383cea3660b7e70a145d7e1
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2020-12-30 08:51:17 +1100

    pushing some chnages

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,12 +5,12 @@ class Board extends React.Component{
     constructor(){
         super()
         this.state = {
-            value: "X",
+            squares: Array(9).fill(null)
         }
     }
 
 renderSquare(i){
-    return <Square value={this.state.value}/>
+    return <Square value={this.state.squares[i]} onClick={this.handleClick(i)}/>
 }
 
     render(){
